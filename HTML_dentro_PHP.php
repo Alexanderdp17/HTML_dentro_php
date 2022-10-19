1d29cc4b5b07886714dd0834edb8e994612e4996
Alexanderdp17 <0200721054@udh.edu.pe>
2022-10-19 18:43:24 -0500

    Second commit

---
+++ HTML_dentro_PHP.php
@@ -0,0 +1,10 @@
+<?php
+	echo"<h1>Hello World</h1>";
+	echo "hola mundo";
+	echo 	"<select>
+			<option>Uno</option>
+			<option>Dos</option>
+			<option>Tres</option>
+			<option>Tres</option>
+			</select>";
+?>
